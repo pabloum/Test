c4b9a5c08fcbd9ba8bb4b4de367d2928bad8e7ce
pabloum <pablo.um00@gmail.com>
2021-09-12 21:47:25 -0500

    Add post deployment script

---
+++ BackEnd/WebApplication/DatabaseProject/Script.PostDeployment1.sql
@@ -0,0 +1,47 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+BEGIN
+  IF NOT EXISTS (SELECT Id FROM [dbo].[User] WHERE Id = 1)
+  BEGIN
+    insert into [dbo].[User] values (1, 'Pablo', 'Uribe', 'puribe')
+  END
+END
+
+
+BEGIN
+  IF NOT EXISTS (SELECT Id FROM [dbo].[User] WHERE Id = 2)
+  BEGIN
+    INSERT INTO [dbo].[User] VALUES (2, 'Maria', 'Rodriguez', 'mrodriguez')
+  END
+END
+
+BEGIN
+  IF NOT EXISTS (SELECT Id FROM [dbo].[User] WHERE Id = 3)
+  BEGIN
+    INSERT INTO [dbo].[User] VALUES (3, 'Vero', 'Abrams', 'vabrams')
+  END
+END
+
+BEGIN
+  IF NOT EXISTS (SELECT Id FROM [dbo].[User] WHERE Id = 4)
+  BEGIN
+    INSERT INTO [dbo].[User] VALUES (4, 'Cristiano', 'Ronaldo', 'serresiete')
+  END
+END
+
+BEGIN
+  IF NOT EXISTS (SELECT Id FROM [dbo].[User] WHERE Id = 5)
+  BEGIN
+    INSERT INTO [dbo].[User] VALUES (5, 'Lionel', 'Messi', 'Liomessi')
+  END
+END
