76f5b45f2d32f6f3dfd9fc3c18047d1f70304186
pabloum <pablo.um00@gmail.com>
2021-09-13 20:34:20 -0500

    Redirect on Submit Create user form

--- FrontEnd/web-users-page/src/App.js
+++ FrontEnd/web-users-page/src/App.js
@@ -5,7 +5,7 @@ import Input from './components/Input'
 import Form from './components/NewUserForm'
 import UserDetails from './components/UserDetails'
 import usersQuemados from './Util/usersMock'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, useHistory } from 'react-router-dom'
 
 import { useState, useEffect } from 'react'
 
@@ -14,6 +14,7 @@ const baseUrl = 'https://localhost:44373/';
 
 function App() {
   const [users, setUsers] = useState([])
+  let history = useHistory();
 
   useEffect(async () => {
     const fetchTask = async () => {
@@ -32,6 +33,7 @@ function App() {
     axios.post(baseUrl + 'api/user', user)
       .then(function (response) {
         console.log(response);
+        history.push('/');
       })
       .catch(function (error) {
         console.log(error);
@@ -42,24 +44,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header />
-        <BrowserRouter>
-          <Switch>
+        <Switch>
 
-            <Route path="/CreateUser">
-              <Form onAdd={createUser} />
-            </Route>
+          <Route path="/CreateUser">
+            <Form onAdd={createUser} />
+          </Route>
 
-            <Route path="/User/ID">
-              <UserDetails />
-            </Route>
+          <Route path="/User/ID">
+            <UserDetails />
+          </Route>
 
-            <Route path="/">
-              <Link to="/CreateUser"><Button /></Link>
-              <Input users={users} />
-            </Route>
+          <Route path="/">
+            <Link to="/CreateUser"><Button /></Link>
+            <Input users={users} />
+          </Route>
 
-          </Switch>
-        </BrowserRouter>
+        </Switch>
       </header>
     </div>
   );
